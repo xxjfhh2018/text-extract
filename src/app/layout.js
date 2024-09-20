3f2a45d81ef1aac6ece00d6d5da21d49b3f879b9
xxjfhh2018 <xxjfhh@126.com>
2024-09-20 22:26:21 +0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,28 +5,20 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata = {
-//   title: {
-//     default: 'Accurate Text Extraction from Images in 2024 | Textract.tools',
-//     template: '%s | Textract.tools',
-//   },
-//   description: 'Textract.tools provides tools to extract text from images or OCR documents or images,Helps you convert images to text.',
-//   keywords: 'Text extraction from images',
-// };
-export async function generateMetadata() {
-  return {
-    title: 'Accurate Text Extraction from Images in 2024 | Textract.tools',
-    description: 'Textract.tools provides tools to extract text from images or OCR documents or images,Helps you convert images to text.',
-    keywords: 'Text extraction from images',
-  };
-}
+export const metadata = {
+  // title: {
+  //   default: 'Accurate Text Extraction from Images in 2024 | Textract.tools',
+  //   template: '%s | Textract.tools',
+  // },
+  title: 'Accurate Text Extraction from Images in 2024 | Textract.tools',
+  description: 'Textract.tools provides tools to extract text from images or OCR documents or images,Helps you convert images to text.',
+  keywords: 'Text extraction from images',
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        
-      </head>
+ 
       <body className={inter.className}>
         <Header />
         <main>{children}</main>

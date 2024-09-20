17b060b7f15f161ea5c56af1e87e39db82438a38
xxjfhh2018 <xxjfhh@126.com>
2024-09-20 17:13:35 +0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,7 @@
-'use client';
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -18,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
         <Header />
         <main>{children}</main>
         <Footer />

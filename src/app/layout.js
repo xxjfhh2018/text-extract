c7066d9f3ecede81bdc18d432f9ee664fa06523c
xxjfhh2018 <xxjfhh@126.com>
2024-09-20 22:00:13 +0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,14 +5,21 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: {
-    default: 'Accurate Text Extraction from Images in 2024 | Textract.tools',
-    template: '%s | Textract.tools',
-  },
-  description: 'Textract.tools provides tools to extract text from images or OCR documents or images,Helps you convert images to text.',
-  keywords: 'Text extraction from images',
-};
+// export const metadata = {
+//   title: {
+//     default: 'Accurate Text Extraction from Images in 2024 | Textract.tools',
+//     template: '%s | Textract.tools',
+//   },
+//   description: 'Textract.tools provides tools to extract text from images or OCR documents or images,Helps you convert images to text.',
+//   keywords: 'Text extraction from images',
+// };
+export async function generateMetadata() {
+  return {
+    title: 'Accurate Text Extraction from Images in 2024 | Textract.tools',
+    description: 'Textract.tools provides tools to extract text from images or OCR documents or images,Helps you convert images to text.',
+    keywords: 'Text extraction from images',
+  };
+}
 
 export default function RootLayout({ children }) {
   return (

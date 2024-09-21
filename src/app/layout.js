3bb749158b9a68146e4e7d9c394c4921d57d0e35
xxjfhh2018 <xxjfhh@126.com>
2024-09-21 16:03:37 +0800

    添加了谷歌统计代码

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,13 +3,13 @@ import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 const inter = Inter({ subsets: ["latin"] });
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export const metadata = {
-  // title: {
-  //   default: 'Accurate Text Extraction from Images in 2024 | Textract.tools',
-  //   template: '%s | Textract.tools',
-  // },
-  title: 'Accurate Text Extraction from Images in 2024 | Textract.tools',
+  title: {
+    default: 'Accurate Text Extraction from Images in 2024 | Textract.tools',
+    template: '%s | Textract.tools',
+  },
   description: 'Textract.tools provides tools to extract text from images or OCR documents or images,Helps you convert images to text.',
   keywords: 'Text extraction from images',
 };
@@ -17,12 +17,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
- 
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+
+      </head>
       <body className={inter.className}>
         <Header />
         <main>{children}</main>
         <Footer />
       </body>
+      <GoogleAnalytics gaId="G-6ZHK5PTNQX" />
     </html>
   );
 }

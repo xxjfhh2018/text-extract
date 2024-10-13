f42d9c8656054aceefefa27d38b5f7e7a177e8b3
xxjfhh2018 <xxjfhh@126.com>
2024-10-13 10:57:14 +0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,12 @@
 import ImageUpExtract from '../components/ImageUpExtract';
 import Faq from '../components/faq';
-import { GoogleTagManager } from '@next/third-parties/google'
-
 export default function Home() {
-
   return (
-    <>   
-    <GoogleTagManager gtmId="G-X5RR5RRQB2" />   
+    <>     
       <ImageUpExtract />
       <section>
         <Faq />
-      </section>
-      
+      </section>      
     </>
   );
 }

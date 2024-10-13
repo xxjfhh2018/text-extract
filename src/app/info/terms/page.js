1bc1c9adc7020187a98c1765bc6552d16c291717
xxjfhh2018 <xxjfhh@126.com>
2024-10-13 15:51:08 +0800

    Update page.js

--- src/app/info/terms/page.js
+++ src/app/info/terms/page.js
@@ -8,16 +8,20 @@ export const metadata = {
   
   export default function TermsOfServicePage() {
 	return (
-	  <div>
-		<h1>Terms of Service</h1>
-		<p>These terms and conditions outline the rules and regulations for the use of Textract.tools, the online tool for extracting text from images and documents.</p>
-		<h2>1. Terms</h2>
-		<p>By accessing this website, we assume you accept these terms and conditions in full. Do not continue to use the website if you do not accept all of the terms and conditions stated on this page.</p>
-		<h2>2. License</h2>
-		<p>Unless otherwise stated, Textract.tools and/or its licensors own the intellectual property rights for all material on this website. All intellectual property rights are reserved.</p>
-		<h2>3. User Responsibilities</h2>
-		<p>Users are responsible for ensuring that any use of Textract.tools complies with the laws and regulations of their jurisdiction.</p>
-	  </div>
+		<section>
+		<div class="mx-auto flex w-full max-w-7xl flex-col items-center justify-center px-5 py-16 md:px-10 md:py-20">
+			<div>
+				<h1>Terms of Service</h1>
+				<p>These terms and conditions outline the rules and regulations for the use of Textract.tools, the online tool for extracting text from images and documents.</p>
+				<h2>1. Terms</h2>
+				<p>By accessing this website, we assume you accept these terms and conditions in full. Do not continue to use the website if you do not accept all of the terms and conditions stated on this page.</p>
+				<h2>2. License</h2>
+				<p>Unless otherwise stated, Textract.tools and/or its licensors own the intellectual property rights for all material on this website. All intellectual property rights are reserved.</p>
+				<h2>3. User Responsibilities</h2>
+				<p>Users are responsible for ensuring that any use of Textract.tools complies with the laws and regulations of their jurisdiction.</p>
+			</div>
+		</div>
+		</section>
 	);
   }
   

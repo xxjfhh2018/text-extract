b47555b36cbe6e67872ae291a443a56aa15dada1
xxjfhh2018 <xxjfhh@126.com>
2024-10-13 10:57:21 +0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,9 +2,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-const inter = Inter({ subsets: ["latin"] });
 import GoogleAnalytics from '../components/GoogleAnalytics.js';
-
+const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: {
     default: 'Accurate Text Extraction from Images in 2024 | Textract.tools',

dc8d01ec1573cb34459a5ab01426f15201b941c2
xxjfhh2018 <xxjfhh@126.com>
2024-10-14 16:37:28 +0800

    增加了how to extract from image

---
+++ src/app/articles/extract-text-from-image/page.js
@@ -0,0 +1,44 @@
+// src/app/info/terms/page.js
+
+export const metadata = {
+	title: 'Extract Text from Image , How to Extract Text from Image| Textract.tools',
+	description: 'Introduces the process of extracting and copying text from images using our online tool Textract.tools, which can be used in the office, word processing, OCR and other fields. ',
+	keywords: ' ',
+  };
+  
+  export default function TermsOfServicePage() {
+	return (
+		<section>
+		<div className="mx-auto flex w-full max-w-5xl flex-col items-center justify-center px-5 md:px-10">
+			<div>
+
+				<h1 className="text-3xl lg:text-4xl font-bold text-black px-5 py-16 md:px-10 text-center ">How to Extract Text from Image</h1>
+                <h2 className="text-2xl font-bold text-black py-6">Introduce</h2>
+				<p>With our powerful online tool, you can extract and copy text from any image in seconds. 
+                    The process is seamless—just upload your image, and Textract.tools will instantly convert it into editable text that you can copy and use for any purpose. 
+                    Our service is perfect for a variety of professional fields such as office work, education, and more.</p>
+				
+				
+				<p></p>
+				
+				<h2 className="text-2xl font-bold text-black py-6">Step 1</h2>
+				<p> Simply drag and drop your image into the box above, r click the 'Select Image to Upload' button to begin.</p>
+				
+				<h2 className="text-2xl font-bold text-black py-6">Step 2</h2>
+				<p>Next, click the 'Extract Text' button and wait patiently for the process to complete.</p>
+				
+				<h2 className="text-2xl font-bold text-black py-6">Step 3</h2>
+				<p>Finally, the text box below will display the extracted text. 
+                    You can click the 'Copy Text' button to copy it.</p>
+
+				
+
+				<h2 className="text-2xl font-bold text-black py-6">In conclusion</h2>
+				<p>Explore the possibilities and increase your productivity with Textract.tools，
+                    your go-to resource for extracting text from images efficiently and effortlessly.</p>
+			</div>
+		</div>
+		</section>
+	);
+  }
+  
